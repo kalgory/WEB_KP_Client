ddc6e3bd3e5e1dcaca083b32d3a10bada7e11869
Hyunmin Jung <jung@hyunmin.dev>
2022-02-05 11:33:38 +0900

    feat: 리사이저 컴포넌트로 리사이징 기능 구현

--- src/components/atoms/ProblemContent/index.jsx
+++ src/components/atoms/ProblemContent/index.jsx
@@ -1,10 +1,6 @@
-import { Typography } from 'antd';
-
-const { Paragraph } = Typography;
-
 function ProblemContent() {
   return (
-    <Paragraph>
+    <p>
       In the process of internal desktop applications development, many different design specs and
       implementations would be involved, which might cause designers and developers difficulties and
       duplication and reduce the efficiency of development. After massive project practice and
@@ -12,7 +8,7 @@ function ProblemContent() {
       Team, which aims to uniform the user interface specs for internal background projects, lower
       the unnecessary cost of design differences and implementation and liberate the resources of
       design and front-end development.
-    </Paragraph>
+    </p>
   );
 }
 
--- src/pages/problems/[id].jsx
+++ src/pages/problems/[id].jsx
@@ -1,25 +1,31 @@
+import { useRef } from 'react';
 import { useRouter } from 'next/router';
 import { PageHeader } from 'antd';
 
 import CodeEditor from 'src/components/atoms/CodeEditor';
 import ProblemContent from 'src/components/atoms/ProblemContent';
+import ProblemFooter from 'src/components/molecules/ProblemFooter';
 import ProblemResizer from 'src/components/molecules/ProblemResizer';
 
 import styles from 'src/styles/pages/problem/styles.module.css';
-import ProblemFooter from '../../components/molecules/ProblemFooter';
 
 function Problem() {
   const router = useRouter();
+  const problemContentContainerRef = useRef(null);
+
+  const adjustSize = (contentWidth) => {
+    problemContentContainerRef.current.style.width = `${contentWidth}px`;
+  };
 
   return (
     <div className={styles.wrapper}>
       <PageHeader onBack={() => router.back()} title='다리 건너기' subTitle='실버3' />
       <main className={styles.main}>
-        <div className={styles.problemContent}>
+        <div className={styles.problemContent} ref={problemContentContainerRef}>
           <ProblemContent />
         </div>
         <div>
-          <ProblemResizer />
+          <ProblemResizer onResize={adjustSize} />
         </div>
         <div className={styles.codeEditor}>
           <CodeEditor />
