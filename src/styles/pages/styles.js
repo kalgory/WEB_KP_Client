7b666901549cbcffe2e1a5204924346ba594c975
Hyunmin Jung <jung@hyunmin.dev>
2022-02-12 21:25:54 +0900

    style: 타이머 절대 높이로 위치 수정

--- src/styles/pages/styles.js
+++ src/styles/pages/styles.js
@@ -2,10 +2,12 @@ import styled from 'styled-components';
 
 // eslint-disable-next-line import/prefer-default-export
 export const Center = styled.div`
-  display: flex;
   position: absolute;
-  width: 100%;
-  height: 100%;
-  justify-content: center;
-  align-items: center;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+  top: 50%;
+  transform: translateY(-50%);
 `;
