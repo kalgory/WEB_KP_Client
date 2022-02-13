e4e507acee8a2e81c3f80d468ad7143c438b61d6
Hyunmin Jung <jung@hyunmin.dev>
2022-02-13 16:44:55 +0900

    feat: Portal api로 모달 최적화

--- src/components/molecules/Modal/index.js
+++ src/components/molecules/Modal/index.js
@@ -1,16 +1,19 @@
+import { createPortal } from 'react-dom';
+
 import ModalInput from 'src/components/atoms/ModalInput';
 
 import { Background, Content } from './styles';
 
 function Modal({ onClose, onConfirm }) {
-  return (
+  return createPortal(
     <>
       <Background onClick={onClose} />
       <Content>
         <ModalInput />
         <ModalInput />
       </Content>
-    </>
+    </>,
+    document.getElementById('modal'),
   );
 }
 
