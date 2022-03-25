c5b88a2a31cc6eaed62fbf386af0b139991bb1dc
Hyunmin Jung <jung@hyunmin.dev>
2022-03-25 20:10:25 +0900

    feat: 모달 시스템 개편

--- src/components/organisms/ModalProvider/index.jsx
+++ src/components/organisms/ModalProvider/index.jsx
@@ -1,5 +1,27 @@
+import { useState } from 'react';
+import dynamic from 'next/dynamic';
+import PropTypes from 'prop-types';
+
+import ModalDispatchContext from '@/contexts/ModalDispatchContext';
+
+const AuthModal = dynamic(() => import('@/components/molecules/AuthModal'), {
+  ssr: false,
+});
+
 function ModalProvider({ children }) {
-  return children;
+  const [modal, setModal] = useState({ isShow: false });
+  const closeModal = () => setModal({ ...modal, isShow: false });
+
+  return (
+    <ModalDispatchContext.Provider value={setModal}>
+      {modal.isShow && <AuthModal onClose={closeModal} />}
+      {children}
+    </ModalDispatchContext.Provider>
+  );
 }
+
+ModalProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default ModalProvider;
